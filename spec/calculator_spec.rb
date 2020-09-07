9ce4adeb655d558e97a01e0eb37ebf4bb9807585
Kim Nguyen <nnhk23@gmail.com>
2020-09-07 21:26:22 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 # Code your solution in calculator.rb
-first_number = 1
-describe "./calculator.rb" do
 
+describe "./calculator.rb" do
+first_number = 1
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
